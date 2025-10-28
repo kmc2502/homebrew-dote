9eab037b85e2792ba54c11582e7146d26486f0dd
2502 <kmc2502@kamiyama.ac.jp>
2025-10-28 11:34:10 +0900

    Update dote.rb

--- dote.rb
+++ dote.rb
@@ -2,7 +2,7 @@ class Dote < Formula
   desc "dot e command"
   homepage "https://github.com/kmc2502/homebrew-dote"
   url "https://raw.githubusercontent.com/kmc2502/homebrew-dote/main/dote.c"
-  sha256 "64a21d15edddf4c432a89a171cc7bdcd453336f0bf2d019618d85b1008c12c99"
+  sha256 "899280347657677ea9dbe4c93ca880fcdf6caab7e2cf745f5882601a63495e5a"
   version "1.0.0"
 
   def install
